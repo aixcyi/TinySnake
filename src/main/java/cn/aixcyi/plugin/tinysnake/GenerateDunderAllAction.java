7596a7657f70619cd1f8eefcf06cf3af2094e6d4
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2023-07-19 16:00:40 +0800

    feat: 实现代码插入功能

--- src/main/java/cn/aixcyi/plugin/tinysnake/GenerateDunderAllAction.java
+++ src/main/java/cn/aixcyi/plugin/tinysnake/GenerateDunderAllAction.java
@@ -2,15 +2,17 @@ package cn.aixcyi.plugin.tinysnake;
 
 import com.intellij.icons.AllIcons.Nodes;
 import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.popup.JBPopup;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.openapi.util.Pair;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.ui.ColoredListCellRenderer;
-import com.intellij.util.Consumer;
 import com.jetbrains.python.PythonLanguage;
 import com.jetbrains.python.psi.*;
+import com.jetbrains.python.psi.impl.PyElementGeneratorImpl;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
@@ -41,10 +43,11 @@ public class GenerateDunderAllAction extends AnAction {
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent event) {
+        Project project = event.getProject();
         PsiFile psi = event.getData(CommonDataKeys.PSI_FILE);
-        if (psi == null) return;
+        if (project == null || psi == null) return;
         PyFile file = (PyFile) psi;
-        DunderAllEntity all = new DunderAllEntity(file.getStatements());  // 遍历所有顶层表达式获取所有符号
+        DunderAllEntity all = new DunderAllEntity(file);  // 遍历所有顶层表达式获取所有符号
 
         // TODO:
         //  JBList<Pair<String, Icon>> choices = new JBList<>(symbols);
@@ -65,13 +68,7 @@ public class GenerateDunderAllAction extends AnAction {
                         this.setEnabled(!all.exports.contains(value));
                     }
                 })
-                .setItemsChosenCallback(new Consumer<Set<? extends String>>() {
-                    @Override
-                    public void consume(Set<? extends String> choices) {
-                        // TODO: choices - exports 之后才是用户可以添加的
-                        choices.forEach(System.out::println);
-                    }
-                })
+                .setItemsChosenCallback(choices -> all.adds(choices, project))
                 .setAdText("Ctrl+单击 选择多个，Shift+单击 批量选择")
                 .setTitle("选择导出到 __all__ 的符号")
                 .createPopup();
@@ -81,15 +78,15 @@ public class GenerateDunderAllAction extends AnAction {
 }
 
 class DunderAllEntity {
-    public String VAR_NAME = "__all__";  //  __all__ 变量的名称
+    public static String VAR_NAME = "__all__";  //  __all__ 变量的名称
     public PyExpression varValue = null;  // __all__ 变量的值
     public List<Icon> icons = new ArrayList<>();  // symbols 原对象的类型对应的符号
     public List<String> symbols = new ArrayList<>();  // 顶层所有符号
     public List<String> exports = new ArrayList<>();  // __all__ 导出的所有符号
 
-    public DunderAllEntity(List<PyStatement> statements) {
-        statements.forEach(this::add);
-        export();
+    public DunderAllEntity(PyFile file) {
+        file.getStatements().forEach(this::add);
+        reexport();
     }
 
     private void add(PyStatement statement) {
@@ -114,7 +111,7 @@ class DunderAllEntity {
                 if (Objects.equals(varName, VAR_NAME)) {
                     varValue = target.second;
                 }
-                // 顶层的普通变量
+                // 顶层除 __all__ 以外的普通变量
                 else {
                     symbols.add(varName);
                     icons.add(Nodes.Variable);
@@ -124,10 +121,35 @@ class DunderAllEntity {
         // TODO: else if (statement instanceof PyIfStatement) {}
     }
 
-    private void export() {
-        if (this.varValue == null) return;
+    /**
+     * 重新导出 __all__ 变量的值的所有子元素。
+     */
+    private void reexport() {
+        exports.clear();
+        if (varValue == null) return;
         for (PsiElement child : varValue.getChildren())
             if (child instanceof PyStringLiteralExpression string)
                 exports.add(string.getStringValue());
+    }
+
+    /**
+     * 往 __all__ 变量的值添加“字符串”。
+     *
+     * @param project 项目。
+     * @param items   所有需要添加的"字符串"。
+     */
+    public void adds(@NotNull final Set<? extends String> items, @NotNull Project project) {
+        reexport();
+        PyElementGeneratorImpl generator = new PyElementGeneratorImpl(project);
+        exports.forEach(items::remove);
+        if (varValue == null) {
+            items.forEach(System.out::println);
+            // TODO: 创建 __all__ 变量。
+        }
+        WriteCommandAction.runWriteCommandAction(project, () -> {
+            for (String item : items) {
+                varValue.add(generator.createStringLiteralFromString(item));
+            }
+        });
     }
 }
