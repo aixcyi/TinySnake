2de5ffc17c1335d4e11d2c2aabce8c3de4ad6348
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2023-07-20 00:35:33 +0800

    fix: 简化 dunder all 的获取

--- src/main/java/cn/aixcyi/plugin/tinysnake/DunderAllEntity.java
+++ src/main/java/cn/aixcyi/plugin/tinysnake/DunderAllEntity.java
@@ -4,7 +4,6 @@ import com.intellij.icons.AllIcons;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Pair;
-import com.intellij.psi.PsiElement;
 import com.jetbrains.python.psi.*;
 import com.jetbrains.python.psi.impl.PyElementGeneratorImpl;
 import org.jetbrains.annotations.NotNull;
@@ -20,11 +19,11 @@ public class DunderAllEntity {
     public PyExpression varValue = null;  // __all__ 变量的值
     public List<Icon> icons = new ArrayList<>();  // symbols 原对象的类型对应的符号
     public List<String> symbols = new ArrayList<>();  // 顶层所有符号
-    public List<String> exports = new ArrayList<>();  // __all__ 导出的所有符号
+    public List<String> exports;  // __all__ 导出的所有符号
 
-    public DunderAllEntity(PyFile file) {
+    public DunderAllEntity(@NotNull PyFile file) {
         file.getStatements().forEach(this::collect);
-        reexport();
+        exports = file.getDunderAll();
     }
 
     /**
@@ -49,16 +48,9 @@ public class DunderAllEntity {
             // 所以需要用循环来提取普通变量
             for (Pair<PyExpression, PyExpression> target : assignment.getTargetsToValuesMapping()) {
                 String varName = target.first.getName();
-
-                // 顶层的 __all__ 变量
-                if (Objects.equals(varName, VAR_NAME)) {
-                    varValue = target.second;
-                }
-                // 顶层除 __all__ 以外的普通变量
-                else {
-                    symbols.add(varName);
-                    icons.add(AllIcons.Nodes.Variable);
-                }
+                if (Objects.equals(varName, VAR_NAME)) return;  // 跳过顶层的 __all__ 变量
+                symbols.add(varName);
+                icons.add(AllIcons.Nodes.Variable);
             }
         }
         // 判断语句
@@ -70,24 +62,12 @@ public class DunderAllEntity {
     }
 
     /**
-     * 重新导出 __all__ 变量的值的所有子元素。
-     */
-    private void reexport() {
-        exports.clear();
-        if (varValue == null) return;
-        for (PsiElement child : varValue.getChildren())
-            if (child instanceof PyStringLiteralExpression string)
-                exports.add(string.getStringValue());
-    }
-
-    /**
      * 往 __all__ 变量的值添加“字符串”。
      *
      * @param project 项目。
      * @param items   所有需要添加的"字符串"。
      */
     public void adds(@NotNull final Set<? extends String> items, @NotNull Project project) {
-        reexport();
         PyElementGeneratorImpl generator = new PyElementGeneratorImpl(project);
         exports.forEach(items::remove);
         if (varValue == null) {
