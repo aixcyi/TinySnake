4ad2e9e46f2eb947c3b5ea1cb5d33ab7e32283c6
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2023-07-24 17:29:44 +0800

    refactor: 优化 "优化 __all__" 对话框样式

--- src/main/java/cn/aixcyi/plugin/tinysnake/DunderAllOptimizerDialog.java
+++ src/main/java/cn/aixcyi/plugin/tinysnake/DunderAllOptimizerDialog.java
@@ -8,38 +8,24 @@ import java.awt.*;
 
 public class DunderAllOptimizerDialog extends DialogWrapper {
     private JPanel contentPanel;
-    private JRadioButton radioNatureOrder;
     private JRadioButton radioAlphabetOrder;
     private JRadioButton radioCharOrder;
-    private JRadioButton radioFullFill;
     private JRadioButton radioStayAlone;
 
     public DunderAllOptimizerDialog() {
         super(true);
-        ButtonGroup groupOrdering = new ButtonGroup();
-        groupOrdering.add(radioAlphabetOrder);
-        groupOrdering.add(radioNatureOrder);
-        groupOrdering.add(radioCharOrder);
-
-        ButtonGroup groupNewLine = new ButtonGroup();
-        groupNewLine.add(radioStayAlone);
-        groupNewLine.add(radioFullFill);
-
         setResizable(false);
         setTitle("优化 __all__");
         init();
     }
 
     public SymbolsOrder getOrdering() {
-        JRadioButton selected;
-        if (radioAlphabetOrder.isSelected()) {
-            selected = radioAlphabetOrder;
-        } else if (radioCharOrder.isSelected()) {
-            selected = radioCharOrder;
-        } else {
-            selected = radioNatureOrder;
-        }
-        return SymbolsOrder.fromLabel(selected.getText());
+        if (radioCharOrder.isSelected())
+            return SymbolsOrder.CHARSET;
+        else if (radioAlphabetOrder.isSelected())
+            return SymbolsOrder.ALPHABET;
+        else
+            return SymbolsOrder.APPEARANCE;
     }
 
     public boolean getSingleLine() {
--- src/main/java/cn/aixcyi/plugin/tinysnake/SymbolsOrder.java
+++ src/main/java/cn/aixcyi/plugin/tinysnake/SymbolsOrder.java
@@ -1,42 +1,18 @@
 package cn.aixcyi.plugin.tinysnake;
 
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.Arrays;
-import java.util.List;
-
 public enum SymbolsOrder {
     /**
      * 按符号出现顺序排序。
      */
-    APPEARANCE("声明顺序"),
+    APPEARANCE,
 
     /**
      * 按字母先后顺序排序（不区分大小写）。
      */
-    ALPHABET("字母顺序"),
+    ALPHABET,
 
     /**
      * 按字母先后顺序排序（区分大小写）。
      */
-    CHARSET("字符编码顺序");
-
-    public final @NotNull String label;
-
-    SymbolsOrder(@NotNull String label) {
-        this.label = label;
-    }
-
-    public static @NotNull List<String> getLabels() {
-        return Arrays.stream(SymbolsOrder.values())
-                .map((enumeration) -> enumeration.label).toList();
-    }
-
-    public static @Nullable SymbolsOrder fromLabel(@NotNull String label) {
-        for (SymbolsOrder enumeration : SymbolsOrder.values())
-            if (enumeration.label.equals(label))
-                return enumeration;
-        return null;
-    }
+    CHARSET
 }
