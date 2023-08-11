c17718e600678a4357081e4356acb0ff573da31c
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2023-08-11 10:38:24 +0800

    feat: 添加创建函数的builder方法

--- src/main/java/cn/aixcyi/plugin/tinysnake/SnippetBuilder.java
+++ src/main/java/cn/aixcyi/plugin/tinysnake/SnippetBuilder.java
@@ -4,6 +4,7 @@ import com.intellij.psi.PsiComment;
 import com.jetbrains.python.psi.*;
 import com.jetbrains.python.psi.impl.PyElementGeneratorImpl;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
@@ -21,6 +22,15 @@ public class SnippetBuilder {
         this.generator = new PyElementGeneratorImpl(file.getProject());
     }
 
+    /**
+     * 拼接序列。
+     *
+     * @param items      序列元素。
+     * @param style      括号风格。
+     * @param lineByLine 每个元素一行，而不是全部挤在同一行。
+     * @param stringify  为每个元素配上双引号。
+     * @return 拼接后的结果字符串。
+     */
     public String makeSequence(@NotNull List<String> items,
                                @NotNull BracketsStyle style,
                                boolean lineByLine,
@@ -31,6 +41,9 @@ public class SnippetBuilder {
 
     /**
      * 创建字符串字面值。
+     *
+     * @param string 字符串内容。不必包含引号。
+     * @return 字符串字面值对象（{@link PyStringLiteralExpression}）。
      */
     public PyStringLiteralExpression cakeString(@NotNull String string) {
         return generator.createStringLiteralFromString(string);
@@ -38,6 +51,9 @@ public class SnippetBuilder {
 
     /**
      * 创建备注。
+     *
+     * @param comment 注释内容。需要包含老头的 "#" 号。
+     * @return 注释对象（{@link PsiComment}）。
      */
     public PsiComment cakeComment(@NotNull String comment) {
         return generator.createFromText(this.version, PsiComment.class, comment);
@@ -46,10 +62,10 @@ public class SnippetBuilder {
     /**
      * 创建列表。
      *
-     * @param items 要添加到列表中的元素。
+     * @param items      要添加到列表中的元素。
      * @param lineByLine 每个元素各占一行。
-     * @param stringify 将所有元素变为字符串字面值。
-     * @return 表达式对象。
+     * @param stringify  将所有元素变为字符串字面值。
+     * @return 列表对象（{@link PyListLiteralExpression}）。
      */
     public PyListLiteralExpression cakeList(@NotNull List<String> items, boolean lineByLine, boolean stringify) {
         return generator.createFromText(
@@ -61,6 +77,10 @@ public class SnippetBuilder {
 
     /**
      * 创建赋值表达式。
+     *
+     * @param variable 变量名
+     * @param value    变量值
+     * @return 赋值语句对象（{@link PyAssignmentStatement}）。
      */
     public PyAssignmentStatement cakeAssignment(@NotNull String variable, @NotNull String value) {
         return generator.createFromText(
@@ -68,5 +88,25 @@ public class SnippetBuilder {
                 PyAssignmentStatement.class,
                 variable + " = " + value
         );
+    }
+
+    /**
+     * 创建函数。
+     *
+     * @param name      函数名。
+     * @param arguments 参数列表。
+     * @param body      函数体。如果留空则默认为 "pass" 。
+     * @param docstring 文档字符串，需要手动添加三双引号和必须的换行。若留空则不会显示。
+     * @return 函数对象（{@link PyFunction}）。
+     */
+    public PyFunction cakeFunction(@NotNull String name,
+                                   @Nullable List<String> arguments,
+                                   @Nullable String body,
+                                   @Nullable String docstring) {
+        var params = arguments == null ? "" : makeSequence(arguments, BracketsStyle.BARE, false, false);
+        var text = "def " + name + "(" + params + "):\n"
+                + (docstring == null ? "" : docstring)
+                + (body == null ? "pass" : body);
+        return this.generator.createFromText(this.version, PyFunction.class, text);
     }
 }
