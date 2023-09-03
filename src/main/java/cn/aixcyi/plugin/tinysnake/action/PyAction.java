88b8b24667e7a1cda316a0b942e4fd025c88ce41
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2023-09-04 01:09:43 +0800

    refactor: 将光标相关操作迁移到 PyAction 中

--- src/main/java/cn/aixcyi/plugin/tinysnake/action/PyAction.java
+++ src/main/java/cn/aixcyi/plugin/tinysnake/action/PyAction.java
@@ -1,8 +1,12 @@
 package cn.aixcyi.plugin.tinysnake.action;
 
 import com.intellij.openapi.actionSystem.*;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.jetbrains.python.psi.PyClass;
 import com.jetbrains.python.psi.PyFile;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import static cn.aixcyi.plugin.tinysnake.Translation.$message;
 
@@ -50,4 +54,31 @@ public abstract class PyAction extends AnAction {
 
     // actionPerformed() 是主业务逻辑所在，是必定要重写的，故设置为抽象。
     public abstract void actionPerformed(@NotNull AnActionEvent event, @NotNull PyFile file);
+
+    /**
+     * 获取编辑器中的光标落在哪一个元素上。
+     *
+     * @param event 消息事件。
+     * @param file  表示 Python 文件的 PSI 元素。
+     * @return PSI元素。若光标不在类内，则返回 null 。
+     */
+    public @Nullable PsiElement getCaretElement(@NotNull AnActionEvent event, @NotNull PyFile file) {
+        var editor = event.getData(CommonDataKeys.EDITOR);
+        if (editor == null) return null;
+        var offset = editor.getCaretModel().getOffset();
+        return file.findElementAt(offset);
+    }
+
+    /**
+     * 获取编辑器中的光标落在哪一个类上。
+     *
+     * @param event 消息事件。
+     * @param file  表示 Python 文件的 PSI 元素。
+     * @return PyClass 对象。若光标不在类内，则返回 null 。
+     */
+    public @Nullable PyClass getCaretClass(@NotNull AnActionEvent event, @NotNull PyFile file) {
+        var element = getCaretElement(event, file);
+        if (element == null) return null;
+        return PsiTreeUtil.getParentOfType(element, PyClass.class);  // 自下而上查找 光标所在符号 所在的类。
+    }
 }
