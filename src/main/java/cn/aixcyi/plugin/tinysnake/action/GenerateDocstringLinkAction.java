93d104ddd81d1b38567d863b82e18648a0d6902c
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2023-09-28 00:23:18 +0800

    feat: 添加 “往 docstring 插入超链接” 的功能

---
+++ src/main/java/cn/aixcyi/plugin/tinysnake/action/GenerateDocstringLinkAction.java
@@ -0,0 +1,60 @@
+package cn.aixcyi.plugin.tinysnake.action;
+
+import cn.aixcyi.plugin.tinysnake.dialog.DocstringLinkCreator;
+import com.intellij.codeInsight.hint.HintManager;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.psi.PsiElement;
+import com.jetbrains.python.PyTokenTypes;
+import com.jetbrains.python.psi.PyFile;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import static cn.aixcyi.plugin.tinysnake.Translation.$message;
+
+/**
+ * 在文档字符串（docstring）中插入超链接。
+ *
+ * @author <a href="https://github.com/aixcyi">砹小翼</a>
+ * @see com.jetbrains.python.psi.impl.PyPlainStringElementImpl
+ */
+public class GenerateDocstringLinkAction extends PyAction {
+
+    @Override
+    public void update(@NotNull AnActionEvent event, @NotNull PyFile file) {
+        event.getPresentation().setVisible(
+                getCaretDocstring(event, file) != null
+        );
+    }
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent event, @NotNull PyFile file) {
+        var hint = HintManager.getInstance();
+        var editor = event.getData(CommonDataKeys.EDITOR);
+        if (editor == null) return;
+        var offset = editor.getCaretModel().getOffset();
+        var project = file.getProject();
+        var document = editor.getDocument();
+        if (getCaretDocstring(event, file) == null) {
+            hint.showInformationHint(editor, $message("hint.GenerateDocstringLinkAction.notfound"));
+            return;
+        }
+        var dialog = new DocstringLinkCreator($message("GenerateDocstringLinkAction.dialog.title"));
+        if (!dialog.showAndGet()) return;
+        var snippet = "`%s <%s>`_".formatted(dialog.getText(), dialog.getLink());
+        WriteCommandAction.runWriteCommandAction(
+                project,
+                $message("command.GenerateDocstringLink"),
+                null,
+                () -> document.insertString(offset, snippet)
+        );
+    }
+
+    @Nullable
+    private PsiElement getCaretDocstring(@NotNull AnActionEvent event, @NotNull PyFile file) {
+        var element = getCaretElement(event, file);
+        if (element == null) return null;
+        return PyTokenTypes.DOCSTRING.equals(element.getNode().getElementType()) ? element : null;
+    }
+}
---
+++ src/main/java/cn/aixcyi/plugin/tinysnake/dialog/DocstringLinkCreator.java
@@ -0,0 +1,35 @@
+package cn.aixcyi.plugin.tinysnake.dialog;
+
+import com.intellij.openapi.ui.DialogWrapper;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DocstringLinkCreator extends DialogWrapper {
+    private JPanel contentPane;
+    private JTextField textField;
+    private JTextField linkField;
+
+    public DocstringLinkCreator(String title) {
+        super(true);
+        setResizable(true);
+        setTitle(title);
+        init();
+    }
+
+    public String getText() {
+        return textField.getText();
+    }
+
+    public String getLink() {
+        return linkField.getText();
+    }
+
+    @Override
+    protected @Nullable JComponent createCenterPanel() {
+        JPanel dialogPanel = new JPanel(new BorderLayout());
+        dialogPanel.add(contentPane, BorderLayout.CENTER);
+        return dialogPanel;
+    }
+}
