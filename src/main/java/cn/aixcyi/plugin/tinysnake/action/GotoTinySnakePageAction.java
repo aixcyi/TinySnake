04f4822fc9eeb2737072e57a2f93bd66be9a186f
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2023-12-25 17:48:25 +0800

    fix: 修复 GotoTinySnakeRelease 引起的报错，解决其打不开链接的问题

--- src/main/java/cn/aixcyi/plugin/tinysnake/action/GotoTinySnakePageAction.java
+++ src/main/java/cn/aixcyi/plugin/tinysnake/action/GotoTinySnakePageAction.java
@@ -1,15 +1,14 @@
 package cn.aixcyi.plugin.tinysnake.action;
 
 import com.intellij.ide.BrowserUtil;
+import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.editor.Editor;
-import com.jetbrains.python.psi.PyFile;
 import org.jetbrains.annotations.NotNull;
 
-public class GotoTinySnakePageAction extends PyAction {
+public class GotoTinySnakePageAction extends AnAction {
 
     @Override
-    public void actionPerformed(@NotNull AnActionEvent event, @NotNull PyFile file, @NotNull Editor editor) {
+    public void actionPerformed(@NotNull AnActionEvent event) {
         BrowserUtil.browse("https://github.com/aixcyi/TinySnake/releases");
     }
 }
