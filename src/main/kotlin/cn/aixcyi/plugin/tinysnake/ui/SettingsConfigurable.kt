b757527e8dc2eee62c7d63678a366dbbf27640e9
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2024-06-21 15:16:19 +0800

    refactor: 使用 UI DSL 重构设置页面

--- src/main/kotlin/cn/aixcyi/plugin/tinysnake/ui/SettingsConfigurable.kt
+++ src/main/kotlin/cn/aixcyi/plugin/tinysnake/ui/SettingsConfigurable.kt
@@ -2,53 +2,128 @@ package cn.aixcyi.plugin.tinysnake.ui
 
 import cn.aixcyi.plugin.tinysnake.storage.Settings
 import cn.aixcyi.plugin.tinysnake.util.IOUtil.message
-import com.intellij.openapi.components.PersistentStateComponent
+import cn.aixcyi.plugin.tinysnake.util.hFill
+import cn.aixcyi.plugin.tinysnake.util.putExtraAction
+import com.intellij.icons.AllIcons
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.options.SearchableConfigurable
-import javax.swing.JComponent
+import com.intellij.openapi.ui.Messages
+import com.intellij.ui.AnActionButton
+import com.intellij.ui.CollectionListModel
+import com.intellij.ui.ToolbarDecorator
+import com.intellij.ui.components.JBList
+import com.intellij.ui.dsl.builder.panel
+import javax.swing.ListSelectionModel
 
 /**
- * 插件设置的控制器。
- *
- * - 对应的 UI 页面组件是 [SettingsComponent] 。
- * - 该控制器仅编辑「IDE」级别的存储设置（[PersistentStateComponent]）。
+ * Tiny Snake 插件设置。
  *
  * @author <a href="https://github.com/aixcyi">砹小翼</a>
  */
 class SettingsConfigurable : SearchableConfigurable {
 
     companion object {
-        const val ID = "cn.aixcyi.plugin.tinysnake.state"
+        const val ID = "tinysnake.SettingsConfigurable"
     }
 
-    /** 页面组件。设置为 `null` 以释放组件。 */
-    private var component: SettingsComponent? = null
+    private val state = Settings.getInstance().state
+    private val model = CollectionListModel(state.myShebangs)
+    private val list = JBList(model).apply { selectionMode = ListSelectionModel.SINGLE_SELECTION }
 
     override fun getId() = ID
 
     override fun getDisplayName() = message("configurable.TinySnake.display_name")
 
-    override fun createComponent(): JComponent {
-        component = SettingsComponent(Settings.getInstance().state)
-        return component!!.mainPanel
+    override fun createComponent() = panel {
+        group(message("panel.ShebangsPart.title")) {
+            row {
+                cell(createToolbarList()).hFill()
+            }
+        }
     }
+
+    private fun createToolbarList() = ToolbarDecorator.createDecorator(list)
+        .setAddAction {
+            val string = Messages.showInputDialog(
+                message("dialog.InputGenerateShebang.message"),
+                message("dialog.InputGenerateShebang.title"),
+                null
+            )
+            if (string.isNullOrEmpty() || string.isBlank()) return@setAddAction
+            model.add(string)
+            list.selectionModel.leadSelectionIndex = model.size - 1
+        }
+        .setEditAction {
+            val string = Messages.showInputDialog(
+                message("dialog.InputGenerateShebang.message"),
+                message("dialog.InputGenerateShebang.title"),
+                null,
+                model.getElementAt(list.selectedIndex),
+                null
+            )
+            if (string.isNullOrEmpty() || string.isBlank()) return@setEditAction
+            model.setElementAt(string, list.selectedIndex)
+        }
+        .setRemoveAction {
+            model.remove(list.selectedIndex)
+            list.selectionModel.leadSelectionIndex = list.leadSelectionIndex
+        }
+        .putExtraAction(object :
+            AnActionButton(message("action.ResetToDefaultConfigs.text"), AllIcons.General.Reset) {
+            override fun getActionUpdateThread() = ActionUpdateThread.BGT
+
+            override fun actionPerformed(e: AnActionEvent) {
+                revert()
+            }
+
+            override fun updateButton(e: AnActionEvent) {
+                val cc = contextComponent
+                e.presentation.isEnabled = cc != null && cc.isShowing && cc.isEnabled && !isOriginal()
+            }
+        })
+        .createPanel()
 
     override fun enableSearch(option: String?): Runnable? {
         // TODO<FUTURE>: 实现搜索
         return null
     }
 
-    override fun isModified() = component?.isModified() ?: false
+    /**
+     * 组件的状态（对于最后一次持久化的状态来说）是否已被修改。
+     */
+    override fun isModified() = model.toList() != state.myShebangs
 
+    /**
+     * 组件的状态是不是（插件预设的）初始状态。
+     */
+    fun isOriginal() = model.toList() == Settings.PRESET_SHEBANGS
+
+    /**
+     * 应用设置。（即把组件中的状态持久化）
+     */
     override fun apply() {
-        component?.apply()
+        with(state) {
+            myShebangs = model.toList()
+        }
     }
 
+    /**
+     * 将组件中的状态重置为最后一次持久化的状态。
+     */
     override fun reset() {
-        component?.reset()
+        model.removeAll()
+        model.addAll(0, state.myShebangs)
+    }
+
+    /**
+     * 将组件的状态还原为（代码预设的）初始状态。
+     */
+    fun revert() {
+        model.removeAll()
+        model.addAll(0, Settings.PRESET_SHEBANGS)
     }
 
     override fun disposeUIResources() {
-        component?.dispose()
-        component = null
     }
 }
--- src/main/kotlin/cn/aixcyi/plugin/tinysnake/util/MeowUiUtil.kt
+++ src/main/kotlin/cn/aixcyi/plugin/tinysnake/util/MeowUiUtil.kt
@@ -1,52 +1,22 @@
 package cn.aixcyi.plugin.tinysnake.util
 
+import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.ui.DialogPanel
 import com.intellij.openapi.ui.validation.DialogValidation
 import com.intellij.openapi.util.text.TextWithMnemonic
-import com.intellij.ui.IdeBorderFactory
+import com.intellij.ui.AnActionButton
+import com.intellij.ui.ToolbarDecorator
 import com.intellij.ui.dsl.builder.Cell
-import org.jetbrains.annotations.ApiStatus
-import javax.swing.*
+import javax.swing.AbstractButton
+import javax.swing.JComponent
+import javax.swing.JLabel
 
 /**
  * 图形界面相关工具。
  *
  * @author <a href="https://github.com/aixcyi">砹小翼</a>
  */
-object MeowUiUtil {
-
-    /**
-     * 检测是否启用了 [NewUI](https://www.jetbrains.com/help/idea/new-ui.html) 。
-     *
-     * - 本方法适用于任意版本的 IntelliJ 平台。
-     * - 232.5150 开始有 `com.intellij.ui.NewUI.isEnabled`。
-     * - 213.2094 开始有 `com.intellij.ui.ExperimentalUI.isNewUI`，属于 [ApiStatus.Internal]。
-     */
-    fun isUsingNewUI(): Boolean = eval {
-        // @since 232.5150.116
-        // https://github.com/JetBrains/intellij-community/commit/ba6df8d944aa1f080d555223917c4b0aa7f43a26
-        // return com.intellij.ui.NewUI.isEnabled()
-        Class.forName("com.intellij.ui.NewUI").getMethod("isEnabled").invoke(null) as Boolean
-    } ?: eval {
-        // @since 213.2094
-        // https://github.com/JetBrains/intellij-community/blob/213.2094/platform/platform-api/src/com/intellij/ui/ExperimentalUI.java
-        // return com.intellij.ui.ExperimentalUI.isNewUI()
-        Class.forName("com.intellij.ui.ExperimentalUI").getMethod("isNewUI").invoke(null) as Boolean
-    } ?: false
-
-    /**
-     * 创建一个带有标题和分割线的 [JPanel] ，常用于设置界面创建分组。
-     *
-     * @param title 标题。提供 `null` 的话会绘制一条从左到右的分割线，提供字符串则从字符串末尾开始绘制。
-     * @return 被修饰过的 [JPanel] ，放置在里面的组件会自动添加左侧缩进。
-     */
-    fun createTitledPanel(title: String?): JPanel {
-        val panel = JPanel()
-        panel.layout = BoxLayout(panel, BoxLayout.Y_AXIS)
-        panel.border = IdeBorderFactory.createTitledBorder(title)
-        return panel
-    }
-}
+object MeowUiUtil
 
 /**
  * 自动解析 Kotlin UI DSL [Cell] 包装的组件的文本，并设置助记键。
@@ -102,5 +72,22 @@ fun <T : JComponent> Cell<T>.hFill(): Cell<T> {
  */
 fun <T : JComponent> Cell<T>.validate(block: T.() -> DialogValidation?): Cell<T> {
     block(component)?.let { validation(it) }
+    return this
+}
+
+/**
+ * 兼容旧版本的 `ToolbarDecorator.addExtraAction(AnActionButton)`
+ * 和新版本的 `ToolbarDecorator.addExtraAction(AnAction)` 。
+ *
+ * @see [ToolbarDecorator.addExtraAction]
+ */
+fun ToolbarDecorator.putExtraAction(action: AnAction): ToolbarDecorator {
+    exec {
+        javaClass.getMethod("addExtraAction", AnAction::class.java).invoke(this, action)
+    }?.exec {
+        javaClass.getMethod("addExtraAction", AnActionButton::class.java).invoke(this, action as AnActionButton)
+    }?.apply {
+        throw NoSuchMethodException("ToolbarDecorator#addExtraAction was not found!")
+    }
     return this
 }
